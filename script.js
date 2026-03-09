1469d95bf5213d52f013db085ba6884b1809c40f
Adolfngobeni-coder <adolfngobeni6@gmail.com>
2026-03-09 14:51:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Fade-in animation on scroll
+const elements = document.querySelectorAll('.fade-in');
 
+const showOnScroll = () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+};
+
+window.addEventListener('scroll', showOnScroll);
+showOnScroll();
